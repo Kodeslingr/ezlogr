d16666bb9d69299cea33e314a4ffa35290e87675
metalgear386 <metalgear386@googlemail.com>
2020-06-17 11:55:04 -0500

    Removing dist and build.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='EZlogR',
-    version='0.8dev',
+    version='0.1',
     author='Jeremy Gillespie',
     author_email='jeremy@kodeslingr.com',
-    description='This is a simple logging tool to help devs focus on writing code instead of writing logs.',
     url='http://www.ezlogr.com',
     packages=setuptools.find_packages(),
     license='MIT',
-    long_description=open('README.md').read(),
+    description='This is a simple logging tool to help devs focus on writing code instead of writing logs.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3.6'
 )
