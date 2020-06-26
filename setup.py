7beca19e6510102f2daf23e0d107c14b2b4200e6
metalgear386 <metalgear386@googlemail.com>
2020-06-26 08:37:11 -0500

    Version update.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='EZlogR',
-    version='0.1.0.dev3',
+    version='0.2.0.dev1',
     author='Jeremy Gillespie',
     author_email='jeremy@kodeslingr.com',
     url='http://www.ezlogr.com',
